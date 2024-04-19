9afbb2f41dbd13aa866434452ead2437bb9102dc
Logan Masniuk <logan.masniuk@gmail.com>
2024-04-19 10:23:26 -0700

    Playing around

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 func main() {
 	fmt.Println("hello world")
 	var foo int
+	var derp bool
 	fmt.Println(foo)
+	fmt.Println(derp)
 	utilz.DoStuff()
+
+	var avgTest int = utilz.Average(5, 10, 100)
+	fmt.Println(avgTest)
 }
--- utilz/utilz.go
+++ utilz/utilz.go
@@ -5,3 +5,7 @@ import "fmt"
 func DoStuff() {
 	fmt.Println("Doing Things")
 }
+
+func Average(x, y, z int) int {
+	return (x + y + z) / 3
+}
